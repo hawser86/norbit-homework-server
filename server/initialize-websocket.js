3794efde8fe02f59e1ab22c553cf227bd0474d4e
hawser <hawser86@gmail.com>
2023-11-21 16:16:24 +0100

    send dummy boat positions via websocket

--- server/initialize-websocket.js
+++ server/initialize-websocket.js
@@ -14,4 +14,19 @@ export const initializeWebsocket = (server) => {
       console.log('user disconnected');
     });
   });
+
+  sendDummyBoatPositions(socketIo);
 };
+
+const sendDummyBoatPositions = (socketIo) => {
+  const dummyBoatPositions = [
+    { latitude: 48.21339894, longitude: 20.73998593, heading: 3.470315226 },
+    { latitude: 48.21340378, longitude: 20.73998763, heading: 3.678493726 },
+    { latitude: 48.21341274, longitude: 20.73999099, heading: 3.863217998 }
+  ];
+  let counter = 0
+
+  setInterval(() => {
+    socketIo.emit('boat-position', dummyBoatPositions[counter++ % dummyBoatPositions.length]);
+  }, 1000);
+}
